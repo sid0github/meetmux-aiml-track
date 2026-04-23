e28c21f42aa54ba73758b4de2ec6d4b31d08bd82
sid0github <sid70889@gmail.com>
2026-04-23 19:49:39 +0530

    feat: k-means clustering

---
+++ Scripts/day15/clustering.py
@@ -0,0 +1,30 @@
+from sklearn.cluster import KMeans
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import make_blobs
+
+X, _ = make_blobs(n_samples=300, centers = 5, cluster_std = 0.06, random_state = 0 )
+
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init='k-means++', random_state = 42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1, 11), wcss) 
+plt.title('The Elbow Method') 
+plt.xlabel('Number of clusters') 
+plt.ylabel('WCSS') 
+plt.show()     
+
+kmeans = KMeans(n_clusters = 5, init='k-means++', random_state = 42)
+y_kmeans = kmeans.fit_predict(X) 
+
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1') 
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2') 
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=50, c='yellow', 
+label='Centroids') 
+plt.title('Clusters of Users') 
+plt.legend() 
+plt.show() 
